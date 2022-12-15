94c1d787c7ed1de0c3b288a7b6b9e3843ada6479
igniokaz <21201390+igniokaz@users.noreply.github.com>
2022-12-15 18:38:05 +0100

    Update streamlit_app.py
    added if-else statement for fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-streamlit.title("My parents new healthy diner")
+streamlit.title("My Mom's new healthy diner")
 
 streamlit.header(' Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -24,18 +24,27 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-
+# New Section to display fruitivye api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+      streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalised = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalised)
+
+except URLError as e:
+    streamlit.error?()
+    
+    
+    streamlit.write('The user entered', fruit_choice)
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # take the json version of the response and normalize it
-fruityvice_normalised = pandas.json_normalize(fruityvice_response.json())
 # output it in the screen as a table
-streamlit.dataframe(fruityvice_normalised)
 
 # don't run anything past here while we troubleshoot
 streamlit.stop()
